a9dbafcc764b12050acf47b05090a55872fdda44
Kamil_Ksiazek <kamil.ksiazek@eniro.com>
2016-12-18 09:43:02 +0100

    Adding drink image using EasyImage and UCrop

--- AlkoBuddy/app/src/main/java/com/kamk2k/alkobuddy/view/CreateDrinkFragment.java
+++ AlkoBuddy/app/src/main/java/com/kamk2k/alkobuddy/view/CreateDrinkFragment.java
@@ -1,8 +1,11 @@
 package com.kamk2k.alkobuddy.view;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +17,14 @@ import com.kamk2k.alkobuddy.R;
 import com.kamk2k.alkobuddy.model.DrinkItem;
 import com.kamk2k.alkobuddy.presenter.CreateDrinkPresenter;
 import com.kamk2k.alkobuddy.presenter.dagger.ApplicationComponent;
+import com.kamk2k.alkobuddy.view.utils.ImagePickerDelegate;
 import com.kamk2k.alkobuddy.view.utils.MVPFragmentView;
 import com.robinhood.ticker.TickerView;
+import com.squareup.picasso.Picasso;
 
 import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar;
+
+import java.io.File;
 
 import javax.inject.Inject;
 
@@ -29,6 +36,8 @@ import io.realm.Realm;
  * Created by PC on 2015-02-23.
  */
 public class CreateDrinkFragment extends MVPFragmentView implements CreateDrinkView {
+
+    private static final String TAG = "CreateDrinkFragment";
 
     // TODO: 15.11.16 add new drink and delete button
     @BindView(R.id.beerView)
@@ -72,6 +81,22 @@ public class CreateDrinkFragment extends MVPFragmentView implements CreateDrinkV
 
     @Inject
     CreateDrinkPresenter presenter;
+    @Inject
+    ImagePickerDelegate imagePickerDelegate;
+
+    private ImagePickerDelegate.OnCompleteListener onImagePickerCompleteListener = new ImagePickerDelegate.OnCompleteListener() {
+        @Override
+        public void onImageReturned(Uri imageUri) {
+            String imagePath = imageUri.getPath();
+            Picasso.with(getContext()).load(new File(imagePath)).error(R.drawable.beer_icon).into(beerView);
+            // TODO: 18.12.16 adding image path to DrinkItem
+        }
+
+        @Override
+        public void onError() {
+            Log.e(TAG, "ImagePickerDelegate.OnCompleteListener error");
+        }
+    };
 
     public CreateDrinkFragment() {
     }
@@ -84,7 +109,9 @@ public class CreateDrinkFragment extends MVPFragmentView implements CreateDrinkV
         initializeSeekBarConnectors();
         setNameChangeListener();
         beerView.setOnClickListener(view -> {
-// TODO: 17.12.16 adding user image
+            imagePickerDelegate
+                    .addOnCompleteListener(onImagePickerCompleteListener)
+                    .startImagePicker(this);
         });
         return rootView;
     }
@@ -189,5 +216,12 @@ public class CreateDrinkFragment extends MVPFragmentView implements CreateDrinkV
         vodkaSeekBarConnector.setCurrentValue(drinkItem.getVodkaVolume());
         customVolumeSeekBarConnector.setCurrentValue(drinkItem.getCustomVolume());
         customPercentageSeekBarConnector.setCurrentValue((int) (100 * drinkItem.getCustomPercentage()));
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        imagePickerDelegate.handleActivityResult(getActivity(), this, requestCode, resultCode, data,
+                presenter.getCurrentDrinkItem().getId());
     }
 }
